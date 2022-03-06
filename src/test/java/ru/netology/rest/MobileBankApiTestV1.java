e0e59db7cd5a58b5a591b9ca846d405d26793620
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2022-03-06 22:21:11 +0300

    Updated

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,41 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("{\n" +
+                        "\"birthday\": \"13.06.1999\",\n" +
+                        "\"name\": \"Иван\",\n" +
+                        "\"passport\": \"4444 № 44444444\",\n" +
+                        "\"patronymic\": \"Иванович\",\n" +
+                        "\"phone\": \"+7 (999)-999-99-99\",\n" +
+                        "\"surname\": \"Иванов\"\n" +
+                        "}") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("{\n" +
+                        "\"birthday\": \"13.06.1999\",\n" +
+                        "\"name\": \"Иван\",\n" +
+                        "\"passport\": \"4444 № 44444444\",\n" +
+                        "\"patronymic\": \"Иванович\",\n" +
+                        "\"phone\": \"+7 (999)-999-99-99\",\n" +
+                        "\"surname\": \"Иванов\"\n" +
+                        "}"))
+        ;
     }
 }
